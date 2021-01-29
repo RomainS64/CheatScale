76b5a1bb58b0f92ae6577c1eb6eef40c471dec93
sohny64 <m.figueiredo64@outlook.fr>
2021-01-29 19:11:24 +0100

    Ajout de l'interface graphique 3/3

--- app/src/main/java/com/example/main/MainActivity.java
+++ app/src/main/java/com/example/main/MainActivity.java
@@ -11,8 +11,9 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.detectionnote.R;
-import com.example.detectionnote.ScaleFinder;
-import com.example.accordeur.Accordeur;
+import com.example.detectionnote.*;
+import com.example.accordeur.*;
+import com.example.metronome.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -55,15 +56,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        /*boutonMetronome.setOnClickListener(new View.OnClickListener() {
+        boutonMetronome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent openMetronome = new Intent(MainActivity.this, MetronomeActivity.class); //Declaration de l'activité pour ouvrir la page Metronome
+                Intent openMetronome = new Intent(MainActivity.this, Metronome.class); //Declaration de l'activité pour ouvrir la page Metronome
                 startActivity(openMetronome); //Ouverture de la page Metronome
 
             }
-        });*/
+        });
     }
 
 
