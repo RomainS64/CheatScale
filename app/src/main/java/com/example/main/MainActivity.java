870a4712046a02efa9f16357617dfb27ca459512
RomainS <salha.romain64@gmail.com>
2021-01-30 00:44:13 +0100

    création de la V0.1 du métronome

--- app/src/main/java/com/example/main/MainActivity.java
+++ app/src/main/java/com/example/main/MainActivity.java
@@ -11,9 +11,10 @@ import android.view.View;
 import android.widget.Button;
 
 import com.example.R;
-import com.example.detectionnote.*;
+import com.example.scalefinder.*;
 import com.example.accordeur.*;
 import com.example.metronome.*;
+import com.example.scalefinder.detectionnote.ScaleFinder;
 
 public class MainActivity extends AppCompatActivity {
 
