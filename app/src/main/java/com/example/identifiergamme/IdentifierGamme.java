e9d39608b81bb5124fc9f4e9bdf35ccf03d2ab1c
Tanguy Boscals de Réals <tbdreals@gmail.com>
2021-01-30 18:39:48 +0100

    Affichage des notes identifiées dans la fonction "Afficher une tonalité"

--- app/src/main/java/com/example/identifiergamme/IdentifierGamme.java
+++ app/src/main/java/com/example/identifiergamme/IdentifierGamme.java
@@ -1,23 +1,29 @@
 package com.example.identifiergamme;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.example.R;
-import com.example.main.MainActivity;
+import com.example.Note;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class IdentifierGamme extends AppCompatActivity {
 
     Button start_button_identifier_tonalite, stop_button_identifier_tonalite;
-    TextView texte_ecoute, texte_jouez, texte_tonalites_compatibles;
+    TextView texte_ecoute, texte_jouez, texte_tonalites_compatibles, texte_note;
     ProgressBar progress_bar;
     View barre_horizontale_3, barre_horizontale_2;
+    ImageView img_check;
+
+    com.example.scalefinder.detectionnote.ManagerAudio audioManager;
+    com.example.scalefinder.detectionnote.ManagerNote noteManager;
+
+    Thread AfficherNote;
+    boolean stopAff = false;
 
 
     @Override
@@ -34,7 +40,11 @@ public class IdentifierGamme extends AppCompatActivity {
         progress_bar = (ProgressBar)findViewById(R.id.progress_bar);
         barre_horizontale_3 = (View)findViewById(R.id.barre_horizontale_3);
         barre_horizontale_2 = (View)findViewById(R.id.barre_horizontale_2);
+        texte_note = (TextView)findViewById(R.id.texte_note);
+        img_check = (ImageView)findViewById(R.id.img_check);
 
+
+        // Déclenchement de la fonctionnalité
         start_button_identifier_tonalite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -50,23 +60,93 @@ public class IdentifierGamme extends AppCompatActivity {
                 start_button_identifier_tonalite.setVisibility(View.INVISIBLE);
                 barre_horizontale_2.setVisibility(View.INVISIBLE);
 
-                stop_button_identifier_tonalite.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        // Retrait des éléments inutiles
-                        texte_ecoute.setVisibility(View.INVISIBLE);
-                        texte_jouez.setVisibility(View.INVISIBLE);
-                        progress_bar.setVisibility(View.INVISIBLE);
-                        barre_horizontale_3.setVisibility(View.INVISIBLE);
-                        stop_button_identifier_tonalite.setVisibility(View.INVISIBLE);
-                        texte_tonalites_compatibles.setVisibility(View.INVISIBLE);
+                //Initialisation des managers
+                audioManager = new com.example.scalefinder.detectionnote.ManagerAudio();
+                noteManager = new com.example.scalefinder.detectionnote.ManagerNote();
 
-                        // Réapparition des éléments utiles
-                        start_button_identifier_tonalite.setVisibility(View.VISIBLE);
-                        barre_horizontale_2.setVisibility(View.VISIBLE);
-                    }
-                });
+                // Initialisation du thread d'affichage de note
+                AfficherNote = new Thread(new Runnable() { public void run() { afficherNote(); } });
+
+                // On lance l'écoute
+                audioManager.run();
+                try {
+                    stopAff = false;
+                    AfficherNote.start();
+                } catch (Throwable t) {
+                    // Message d'erreur à dev
+                }
             }
         });
+
+
+        // Arrêt de la fonctionnalité
+        stop_button_identifier_tonalite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Arrêt du thread
+                stopAff = true;
+                try {
+                    AfficherNote.join();
+                }
+                catch(InterruptedException e) {
+                    // Message d'erreur à dev
+                }
+
+                // Retrait des éléments inutiles
+                texte_ecoute.setVisibility(View.INVISIBLE);
+                texte_jouez.setVisibility(View.INVISIBLE);
+                progress_bar.setVisibility(View.INVISIBLE);
+                barre_horizontale_3.setVisibility(View.INVISIBLE);
+                stop_button_identifier_tonalite.setVisibility(View.INVISIBLE);
+                texte_tonalites_compatibles.setVisibility(View.INVISIBLE);
+                img_check.setVisibility(View.INVISIBLE);
+                texte_note.setVisibility(View.INVISIBLE);
+
+                // Réapparition des éléments utiles
+                start_button_identifier_tonalite.setVisibility(View.VISIBLE);
+                barre_horizontale_2.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+    private void afficherNote() {
+        double frequence;
+
+        while(!stopAff) {
+            try {
+                frequence = audioManager.getFrequenceForte();
+
+                // txt.setText("" + frequence);
+
+                if (noteManager.estUneNote(frequence)) {
+
+                    Note noteProche = noteManager.getNoteLaPlusProche(frequence);
+
+                    // Disparition des indications de recherche
+                    texte_ecoute.setVisibility(View.INVISIBLE);
+                    progress_bar.setVisibility(View.INVISIBLE);
+
+
+                    // Affichage de la note
+                    texte_note.setVisibility(View.VISIBLE);
+                    img_check.setVisibility(View.VISIBLE);
+                    texte_note.setText(noteProche.toString());
+
+
+                } else {
+                    // Apparition des indications de recherche
+                    texte_ecoute.setVisibility(View.VISIBLE);
+                    progress_bar.setVisibility(View.VISIBLE);
+
+
+                    // Disparition de la note
+                    texte_note.setVisibility(View.INVISIBLE);
+                    img_check.setVisibility(View.INVISIBLE);
+                    texte_note.setText("");
+                }
+            }catch(Throwable t){
+                t.printStackTrace();
+            }
+        }
     }
 }
