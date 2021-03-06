e67b01178a32eb6f7a03c86edfaa71df34216dc3
sohny64 <m.figueiredo64@outlook.fr>
2021-03-06 17:02:17 +0100

    Design de Identifier une tonalite

--- app/src/main/java/com/example/accordeur/Accordeur.java
+++ app/src/main/java/com/example/accordeur/Accordeur.java
@@ -17,6 +17,9 @@ public class Accordeur extends AppCompatActivity {
                 SYSTEM_UI_FLAG_FULLSCREEN | SYSTEM_UI_FLAG_HIDE_NAVIGATION   |
                 SYSTEM_UI_FLAG_LAYOUT_STABLE | SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         setContentView(R.layout.accordeur);
+
+
+
     }
 
 }
